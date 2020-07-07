0b9d0ccf26eb42a05c6b86975af02d3c2abd607b
thinhbui2502 <buiducthinh2502@gmail.com>
2020-07-07 09:59:40 +0700

    add file hackernew demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { LikeComponent } from './like/like.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LikeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,13 +1,12 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
 })
-export class AppComponent {
-  title = 'hackernew';
-/*  articles = [
+export class ContentComponent implements OnInit {
+  articles = [
     {
       title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
       url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40'
@@ -24,13 +23,9 @@ export class AppComponent {
       title: 'Let’s Build A Web Server. Part 1.',
       url: 'https://ruslanspivak.com/lsbaws-part1/'
     }
-  ];*/
+  ];
 
-  // updateArticle() {
-  //   this.article.title = document.getElementById('article-title')["value"];
-  //   this.article.url = document.getElementById('article-url')["value"];
-  // }
- /* private news: { title: string; url: string };
+  private news: { title: string; url: string };
 
   addArticle() {
     this.news = {
@@ -40,6 +35,11 @@ export class AppComponent {
     this.news.title = document.getElementById('article-title')["value"]
     this.news.url = document.getElementById('article-url')['value']
     this.articles.push(this.news)
-  }*/
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
